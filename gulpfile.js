37c4f959a2f1ea2fd7c5fdb85f1eab5568cc4d9b
SkeLeT-LIGHT <eldorbekm94@gmail.com>
2020-07-31 08:54:06 +0500

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'));
+    gulp.watch('app/scss/*.scss', gulp.parallel('sass'));
     gulp.watch('app/*.html', gulp.parallel('html'));
     gulp.watch('app/js/*.js', gulp.parallel('js'));
 });
