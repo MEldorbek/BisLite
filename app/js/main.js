c20e933f3d7701bd9f72cb181903f3b0cfc4b715
SkeLeT-LIGHT <eldorbekm94@gmail.com>
2020-08-03 22:43:44 +0500

    added section works

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,11 @@ $(function() {
         arrows: false,
         dots: true,
     });
+
+    $('.works__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/arrow_left.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arrow_right.svg" alt=""></button>',
+    });
 });
